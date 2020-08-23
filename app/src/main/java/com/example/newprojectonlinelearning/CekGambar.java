2af6d83a901cec78f7ea57fde38053606c45e0a7
ajisuryawan <kazumovie132@gmail.com>
2020-08-23 20:15:00 +0700

    cek

--- app/src/main/java/com/example/newprojectonlinelearning/CekGambar.java
+++ app/src/main/java/com/example/newprojectonlinelearning/CekGambar.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.Target;
+import com.squareup.picasso.Picasso;
 
 public class CekGambar extends AppCompatActivity {
 
@@ -14,10 +15,18 @@ public class CekGambar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cek_gambar);
+
         ImageView iv = (ImageView)findViewById(R.id.ivcobagambar);
-        Glide.with(CekGambar.this)
-                .load("https://live.staticflickr.com/6021/5979551591_e61f575354.jpg")
+        Glide
+                .with(CekGambar.this)
+                .load("https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg")
+                .centerCrop()
                 .placeholder(R.mipmap.ic_launcher)
                 .into(iv);
+//        Picasso.get()
+//                .load("https://live.staticflickr.com/6021/5979551591_e61f575354.jpg")
+//                .placeholder(R.mipmap.ic_launcher)
+//                .error(R.mipmap.ic_launcher)
+//                .into(iv);
     }
 }
