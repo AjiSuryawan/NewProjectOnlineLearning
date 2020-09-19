f3294ef42046aa404e75cdf71984863d6b839770
Aji Suryawan <kazumovie132@gmail.com>
2020-09-19 11:58:22 +0700

    Move Apply Plugin dan save fitur

--- app/src/main/java/com/example/newprojectonlinelearning/DetailMovie.java
+++ app/src/main/java/com/example/newprojectonlinelearning/DetailMovie.java
@@ -3,6 +3,7 @@ package com.example.newprojectonlinelearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -11,7 +12,14 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 public class DetailMovie extends AppCompatActivity {
+    Realm realm;
+    RealmHelper realmHelper;
+    ModelMovieRealm movieModel;
+
 
     Bundle extras;
     String title;
@@ -50,5 +58,26 @@ public class DetailMovie extends AppCompatActivity {
                     .into(ivposter);
             // and get whatever type user account id is
         }
+
+        //Set up Realm
+        Realm.init(DetailMovie.this);
+        RealmConfiguration configuration = new RealmConfiguration.Builder().build();
+        realm = Realm.getInstance(configuration);
+
+
+        btnbookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                movieModel = new ModelMovieRealm();
+                movieModel.setDesc(deskripsi);
+                movieModel.setJudul(title);
+                movieModel.setPath(path);
+                movieModel.setReleaseDate(date);
+
+                realmHelper = new RealmHelper(realm);
+                realmHelper.save(movieModel);
+
+            }
+        });
     }
 }
