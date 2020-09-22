6109c9a267349509255a0e5fc55887b7a470cad4
ajisuryawan <kazumovie132@gmail.com>
2020-09-22 09:30:33 +0700

    cek

--- app/src/main/java/com/example/newprojectonlinelearning/MainMenu.java
+++ app/src/main/java/com/example/newprojectonlinelearning/MainMenu.java
@@ -24,5 +24,11 @@ public class MainMenu extends AppCompatActivity {
             }
         });
         menu2 = (CardView)findViewById(R.id.menu2);
+        menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ListDataFavourite.class));
+            }
+        });
     }
 }
