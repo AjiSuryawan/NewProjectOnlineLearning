8d35409b485d8a3b137a1c338fe78704c15b3634
ajisuryawan <kazumovie132@gmail.com>
2020-10-16 09:58:13 +0700

    update

--- app/src/main/java/com/example/newprojectonlinelearning/RealmHelper.java
+++ app/src/main/java/com/example/newprojectonlinelearning/RealmHelper.java
@@ -44,4 +44,14 @@ public class RealmHelper {
         return results;
     }
 
+    public void delete(Integer id){
+        final RealmResults<ModelMovieRealm> model = realm.where(ModelMovieRealm.class).equalTo("id", id).findAll();
+        realm.executeTransaction(new Realm.Transaction() {
+            @Override
+            public void execute(Realm realm) {
+                model.deleteFromRealm(0);
+            }
+        });
+    }
+
 }
